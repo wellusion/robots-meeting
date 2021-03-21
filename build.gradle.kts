bd2ad945a7be6ccdc4978db7ebc4d426371da25d
well <wellusion@mail.ru>
2021-03-21 15:40:50 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.31"
+    application
+}
+
+group = "com.github.wellusion"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "13"
+}
+
+application {
+    mainClassName = "MainKt"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "robots-meeting"
+
